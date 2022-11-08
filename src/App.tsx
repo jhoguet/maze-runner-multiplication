595f1a012af09fd9aca7a83481ea5398c47f0b02
Jon Hoguet <jon@meraki.inc>
2022-11-07 18:52:59 -0700

    refactor to MeshBuilder

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useRef, useEffect } from "react";
-import { Engine, Scene, FreeCamera, Vector3, HemisphericLight, Mesh } from 'babylonjs';
+import { Engine, Scene, FreeCamera, Vector3, HemisphericLight, Mesh, MeshBuilder } from 'babylonjs';
 
 // TODO: quotes
 // TODO: 4 spaces
@@ -24,11 +24,11 @@ const MazeRunner = () => {
       // Create a basic light, aiming 0, 1, 0 - meaning, to the sky
       new HemisphericLight('light1', new Vector3(0, 1, 0), scene);
       // Create a built-in "sphere" shape; its constructor takes 6 params: name, segment, diameter, scene, updatable, sideOrientation
-      var sphere = Mesh.CreateSphere('sphere1', 16, 2, scene, false, Mesh.FRONTSIDE);
+      const sphere = MeshBuilder.CreateSphere('sphere1', { segments: 16, diameter: 2, sideOrientation: Mesh.FRONTSIDE  }, scene);
       // Move the sphere upward 1/2 of its height
       sphere.position.y = 1;
       // Create a built-in "ground" shape; its constructor takes 6 params : name, width, height, subdivision, scene, updatable
-      Mesh.CreateGround('ground1', 6, 6, 2, scene, false);
+      MeshBuilder.CreateGround('ground1', { width: 6, height: 6, subdivisions: 2}, scene);
       // Return the created scene
       return scene;
     }
