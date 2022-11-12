d7121697991c43de46384dcbfc47422363923201
Jon Hoguet <jon@meraki.inc>
2022-11-12 10:10:57 -0700

    add floor texture

--- src/MazeSceneFactory.ts
+++ src/MazeSceneFactory.ts
@@ -5,6 +5,8 @@ import {
     HemisphericLight,
     Mesh,
     MeshBuilder,
+    StandardMaterial,
+    Texture,
 } from 'babylonjs';
 import { MazeUniversalCameraFactory } from './cameras/MazeUniversalCameraFactory';
 
@@ -29,6 +31,8 @@ export class MazeSceneFactory {
         // Create a basic BJS Scene object
         const scene = new Scene(engine);
         // scene.debugLayer.show({  })
+        (window as any).scene = scene;
+
         this.cameraFactory.createAndAttachCamera({ scene });
 
         // Create a basic light, aiming 0, 1, 0 - meaning, to the sky
@@ -39,8 +43,12 @@ export class MazeSceneFactory {
         // Move the sphere upward 1/2 of its height
         sphere.position.y = 1;
         // Create a built-in "ground" shape; its constructor takes 6 params : name, width, height, subdivision, scene, updatable
-        const ground = MeshBuilder.CreateGround('ground1', { width: 250, height: 250, subdivisions: 2 }, scene);
-        ground.checkCollisions = true
+        const ground = MeshBuilder.CreateGround('ground1', { width: 30, height: 30, subdivisions: 2 }, scene);
+        ground.checkCollisions = true;
+
+        const groundMaterial = new StandardMaterial('groundMaterial');
+        ground.material = groundMaterial;
+        groundMaterial.diffuseTexture = new Texture('https://www.babylonjs-playground.com/textures/floor.png', scene);
         // Return the created scene
         engine.runRenderLoop(function () {
             scene.render();
