62205849df84beb6d7db153137eef4b2b44e3e97
Jon Hoguet <jon@meraki.inc>
2022-11-14 17:13:23 -0700

    refactor method for creating wall

--- src/MazeSceneFactory.ts
+++ src/MazeSceneFactory.ts
@@ -50,7 +50,8 @@ export class MazeSceneFactory {
         // Move the sphere upward 1/2 of its height
         // sphere.position.y = 1;
         // Create a built-in "ground" shape; its constructor takes 6 params : name, width, height, subdivision, scene, updatable
-        const ground = MeshBuilder.CreateGround('ground1', { width: 30, height: 30, subdivisions: 2 }, scene);
+        const groundSize = 30;
+        const ground = MeshBuilder.CreateGround('ground1', { width: groundSize, height: groundSize, subdivisions: 2 }, scene);
         ground.checkCollisions = true;
 
         const groundMaterial = new StandardMaterial('groundMaterial');
@@ -59,17 +60,13 @@ export class MazeSceneFactory {
 
 
         // Create a wall
-        var box = MeshBuilder.CreateBox("crate", { size: 2, height: 13, depth: 13, width: 0 }, scene);
-        const boxMaterial = new StandardMaterial("Mat", scene);
-        box.material = boxMaterial
-        boxMaterial.diffuseTexture = new Texture("https://www.babylonjs-playground.com/textures/crate.png", scene);
-        box.checkCollisions = true;
-
-        var theta = 0;
-        var radius = 6;
-        const boxY = (radius + randomNumber(-0.5 * radius, 0.5 * radius)) * Math.sin(theta + randomNumber(-0.1 * theta, 0.1 * theta));
-        const boxX = (radius + randomNumber(-0.5 * radius, 0.5 * radius)) * Math.cos(theta + randomNumber(-0.1 * theta, 0.1 * theta));
-        box.position = new Vector3(boxX, 6.5, boxY);
+        this.createWall({
+            centerX: 0,
+            centerY: groundSize / 2,
+            width: 30,
+            height: 15,
+            scene
+        });
 
         // Return the created scene
         engine.runRenderLoop(function () {
@@ -79,5 +76,18 @@ export class MazeSceneFactory {
         window.addEventListener('resize', function () {
             engine.resize();
         });
+    }
+
+    private createWall = ({ centerX, centerY, width, height, scene }: { centerX: number, centerY: number, width: number, height: number, scene: Scene }) => {
+        var box = MeshBuilder.CreateBox("crate", { size: 2, height, depth: .5, width }, scene);
+        const boxMaterial = new StandardMaterial("Mat", scene);
+        box.material = boxMaterial
+        boxMaterial.diffuseTexture = new Texture("https://www.babylonjs-playground.com/textures/crate.png", scene);
+        box.checkCollisions = true;
+
+        //
+
+        box.position = new Vector3(centerX, height / 2, centerY);
+
     }
 }
