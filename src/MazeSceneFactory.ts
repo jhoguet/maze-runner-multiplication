3ae29271405008e62fc6497909dd9106438eea44
Jon Hoguet <jon@meraki.inc>
2022-11-17 16:50:19 -0700

    first pass at game engine

--- src/MazeSceneFactory.ts
+++ src/MazeSceneFactory.ts
@@ -12,11 +12,100 @@ import {
     ActionManager,
     Color3
 } from '@babylonjs/core';
-import '@babylonjs/core/Debug/debugLayer'; 
+import '@babylonjs/core/Debug/debugLayer';
 import '@babylonjs/inspector'
-import { Rectangle, AdvancedDynamicTexture, TextBlock, Control } from '@babylonjs/gui/2D';
+import { Rectangle, AdvancedDynamicTexture, TextBlock, Control, Grid, Button } from '@babylonjs/gui/2D';
+import { State } from '@symbiotic/green-state';
 
 import { MazeUniversalCameraFactory } from './cameras/MazeUniversalCameraFactory';
+
+
+const multiplesPerLevel = {
+    '1': [1, 2, 5, 10],
+    '2': [1, 2, 3, 4, 5, 10],
+    '3': [1, 2, 3, 4, 5, 6, 7, 10],
+    '4': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    '5': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
+    '6': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+}
+
+class GameState extends State<{ level: number, correctCount: number, incorrectCount: number, problem?: { left: number, right: number, answer: number} }> {
+    constructor() {
+        super({ level: 1, correctCount: 0, incorrectCount: 0 });
+
+        this.nextProblem();
+    }
+
+    private nextProblem = () => {
+        const multiples = this.getMultiples();
+        const leftIndex = Math.floor(Math.random() * multiples.length);
+        const rightIndex = Math.floor(Math.random() * multiples.length);
+        const left = multiples[leftIndex];
+        const right = multiples[rightIndex];
+        this.setState({
+            problem: {
+                left,
+                right,
+                answer: left * right,
+            }
+        });
+    }
+
+    getOptions = (): number[] => {
+        const options: number[] = [];
+
+        while (options.length <= 4){
+            // random number from -10 to +10
+            const offset = Math.floor(Math.random() * 20) - 10;
+            const candidate = this.state.problem!.answer + offset;
+            if (![...options, this.state.problem!.answer].includes(candidate)){
+                options.push(candidate);
+            }
+        }
+        return options;
+    }
+
+    attemptAnswer = (answer: number) => {
+        const isCorrect = this.state.problem!.answer === answer;
+        if (isCorrect){
+            this.setState({
+                correctCount: this.state.correctCount + 1
+            });
+        }
+        else {
+            this.setState({
+                incorrectCount: this.state.incorrectCount + 1
+            });
+        }
+        this.checkExperience();
+
+        this.nextProblem();
+
+        return isCorrect;
+    }
+
+    checkExperience = () => {
+        if (this.state.correctCount >= 5){
+            this.advance();
+        }
+    }
+
+    getMultiples = (): number[] => {
+        return (multiplesPerLevel as any)[String(this.state.level)];
+    }
+
+    advance = () => {
+        const level = this.state.level;
+        if (level < 6){
+            this.setState({
+                level: level + 1,
+                correctCount: 0,
+                incorrectCount: 0
+            });
+        }
+    }
+}
+(window as any).gameState = new GameState();
 
 var randomNumber = function (min: number, max: number) {
     if (min === max) {
@@ -152,49 +241,49 @@ export class MazeSceneFactory {
 
         this.createGap({
             height: wallHeight,
-            canadate: 10, 
-            width: 5, 
+            canadate: 10,
+            width: 5,
             center: new Vector3(-2.5, wallHeight / 2, 10),
             scene
         });
 
         this.createGap({
             height: wallHeight,
-            canadate: 0, 
-            width: 5, 
+            canadate: 0,
+            width: 5,
             center: new Vector3(4.5, wallHeight / 2, 10),
             scene
         });
 
         this.createGap({
             height: wallHeight,
-            canadate: 6, 
-            width: 5, 
+            canadate: 6,
+            width: 5,
             center: new Vector3(-9.5, wallHeight / 2, 10),
             scene
         });
 
         this.createGap({
             height: wallHeight,
-            canadate: 2, 
-            width: 5, 
+            canadate: 2,
+            width: 5,
             center: new Vector3(-16.5, wallHeight / 2, 10),
             scene
         });
         this.createGap({
             height: wallHeight,
-            canadate: 15, 
-            width: 5, 
+            canadate: 15,
+            width: 5,
             center: new Vector3(11.5, wallHeight / 2, 10),
             scene
         });
 
         //scene.debugLayer.show()
-        
-        
+
+
 
         camera.onCollide = mesh => {
-            if (mesh.name === 'gap'){
+            if (mesh.name === 'gap') {
                 camera.position = new Vector3(0, 5, -28);
             }
         }
@@ -212,11 +301,11 @@ export class MazeSceneFactory {
 
         const advancedTexture = AdvancedDynamicTexture.CreateFullscreenUI('gui');
 
-        
+
 
         // gui test
         const rect = new Rectangle();
-        const text = new TextBlock("text",String(number1) + " x " + String(number2));
+        const text = new TextBlock("text", String(number1) + " x " + String(number2));
         rect.width = .2;
         rect.height = '40px';
         rect.cornerRadius = 20;
@@ -225,6 +314,9 @@ export class MazeSceneFactory {
         rect.addControl(text)
         advancedTexture.addControl(rect);
         rect.verticalAlignment = Control.VERTICAL_ALIGNMENT_TOP
+
+        const stats = this.createGameStats();
+        advancedTexture.addControl(stats);
 
         // Return the created scene
         engine.runRenderLoop(function () {
@@ -261,6 +353,82 @@ export class MazeSceneFactory {
         gapTexture.addControl(gapRect);
     }
 
+    private createGameStats = (): Rectangle => {
+        const stats = new Rectangle();
+        stats.width = '400px';
+        stats.height = '400px';
+        stats.cornerRadius = 10;
+        stats.thickness = 1;
+        stats.background = 'gray';
+        stats.verticalAlignment = Control.VERTICAL_ALIGNMENT_TOP;
+        stats.horizontalAlignment = Control.HORIZONTAL_ALIGNMENT_RIGHT;
+        stats.paddingTop = '10px';
+        stats.paddingRight = '10px';
+        const grid = new Grid();
+        stats.addControl(grid)
+
+        grid.addColumnDefinition(300, true);
+
+        grid.addRowDefinition(60, true)
+        const level = new TextBlock('level', 'Level 1');
+        grid.addControl(level, 0, 0);
+
+        grid.addRowDefinition(60, true)
+        const multiples = new TextBlock('multiples', '1, 2, 5, 10');
+        grid.addControl(multiples, 1, 0);
+
+        grid.addRowDefinition(60, true)
+        const accuracy = new TextBlock('accuracy', 'Accuracy: 100%');
+        grid.addControl(accuracy, 2, 0);
+
+        // grid.addRowDefinition(60, true)
+        // const speed = new TextBlock('speed', '10 problems per minute');
+        // grid.addControl(speed, 3, 0);
+
+        grid.addRowDefinition(60, true)
+        const problem = new TextBlock('problem', '');
+        grid.addControl(problem, 4, 0);
+
+
+        grid.addRowDefinition(60, true)
+        const button1 = Button.CreateSimpleButton('button1', '');
+
+        button1.onPointerClickObservable.add(() => {
+            const value = Number(button1.textBlock!.text);
+            gameState.attemptAnswer(value);
+        })
+        button1.addControl(new TextBlock());
+        grid.addControl(button1, 4, 0);
+
+        grid.addRowDefinition(60, true)
+        const button2 = Button.CreateSimpleButton('button2', '');
+
+        button2.onPointerClickObservable.add(() => {
+            const value = Number(button1.textBlock!.text);
+            gameState.attemptAnswer(value);
+        })
+        button2.addControl(new TextBlock());
+        grid.addControl(button2, 5, 0);
+
+
+        const gameState = ((window as any).gameState as GameState);
+        gameState.subscribe(() => {
+            level.text = `Level ${gameState.state.level}`;
+            multiples.text = gameState.getMultiples().join(', ');
+            problem.text = `${gameState.state.problem!.left} x ${gameState.state.problem!.right}`;
+            if ((gameState.state.correctCount + gameState.state.incorrectCount) > 0){
+                console.log(gameState.state);
+                accuracy.text = `${Math.round(gameState.state.correctCount / (gameState.state.correctCount + gameState.state.incorrectCount) * 100)}% right`
+            } else {
+                accuracy.text = '';
+            }
+
+            button1.textBlock!.text = String(gameState.state.problem!.answer);
+            button2.textBlock!.text = String(gameState.getOptions()[0]);
+        });
+        return stats;
+    }
+
     private createWall = ({ centerX, centerY, width, height, scene, rotationDegrees }: { centerX: number, centerY: number, width: number, height: number, scene: Scene, rotationDegrees: number }) => {
         var box = MeshBuilder.CreateBox("crate", { size: 2, height, depth: .5, width }, scene);
         box.rotation.y = Tools.ToRadians(rotationDegrees);
@@ -269,9 +437,6 @@ export class MazeSceneFactory {
         boxMaterial.diffuseTexture = new Texture("https://www.babylonjs-playground.com/textures/crate.png", scene);
         box.checkCollisions = true;
 
-        //
-
         box.position = new Vector3(centerX, height / 2, centerY);
-
     }
 }
